9bbb2f3133768ef16354f13d8f3184d70572bfcb
Michiru <starffhwxx@gmil.com>
2019-07-28 23:44:57 +0800

    女と男

---
+++ TinderUIProject/CuteViewController.swift
@@ -0,0 +1,58 @@
+//
+//  CuteViewController.swift
+//  TinderUIProject
+//
+//  Created by 星みちる on 2019/07/28.
+//  Copyright © 2019 星みちる. All rights reserved.
+//
+
+import UIKit
+import Koloda
+
+class CuteViewController: UIViewController {
+
+    @IBOutlet weak var kolodaView: KolodaView!
+    let women = ["airi","an","arisu","eraiza","kannna","kyoko","mai","miduki","minami","ngomi","satomi","sayuri","suzu","yui"]
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    @IBAction func back(_ sender: UIButton) {
+        kolodaView.revertAction()
+    }
+    
+    
+    @IBAction func left(_ sender: UIButton) {
+        kolodaView.swipe(.left)
+    }
+
+    @IBAction func right(_ sender: UIButton){
+        kolodaView.swipe(.right)
+        
+    }
+    
+    
+
+   
+
+}
+extension CuteViewController:KolodaViewDelegate,KolodaViewDataSource{
+    func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+        let image = UIImage(named: women[index])
+        
+        let imageview = UIImageView(image:image)
+        
+        return imageview
+    }
+    
+    func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+        return women.count
+        
+    }
+    
+    
+}
---
+++ TinderUIProject/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  TinderUIProject
+//
+//  Created by 星みちる on 2019/07/26.
+//  Copyright © 2019 星みちる. All rights reserved.
+//
+
+import UIKit
+import Koloda
+
+class ViewController: UIViewController {
+  
+    @IBOutlet weak var kolodaView: KolodaView!
+    let men = ["haruma","hukuyama","juniti","kento","kubota","makken","nisijima","okada","ryo","ryouma","souta","suda","syo","syun","takenouti","takeru","tamaki","touri","tumabuki","yagura","yamada"]
+    
+   
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        kolodaView.delegate = self
+        kolodaView.dataSource = self
+        
+    }
+    @IBAction func left(_ sender: UIButton) {
+    kolodaView.swipe(.left)
+    
+    }
+    
+    @IBAction func right(_ sender: UIButton) {
+    kolodaView.swipe(.right)
+        
+    }
+    
+    @IBAction func back(_ sender: UIButton) {
+        kolodaView.revertAction()
+    }
+    
+    
+    
+}
+
+extension ViewController:KolodaViewDelegate,KolodaViewDataSource{
+    func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+        //画像を生成
+        let image = UIImage(named: men[index])
+        
+        //ImageViewに生成した画像を設定
+        let imageView = UIImageView(image: image)
+        
+        //ImageViewを返す
+        return imageView
+        
+    }
+    
+    func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+            return men.count
+    }
+    
+    //GitHUbのページを読み込んで制限があれば書き換えていく
+    func koloda(_ koloda: KolodaView, allowedDirectionsForIndex index: Int) -> [SwipeResultDirection] {
+        return [.left,.right,.up,.down]
+    }
+
+    
+    
+}
